7e5d198b9ef9ed337481bbe87e2551041224a66c
jbjoatton <jbjoatton@gmail.com>
2018-11-24 09:21:40 +0100

    Add js scroll example

---
+++ sketches/05-js-scroll/sketch.js
@@ -0,0 +1,22 @@
+let scroll = 0;
+
+function setup() {
+	var cnv = createCanvas(windowWidth, windowHeight);
+	cnv.parent('sketch');
+}
+
+function draw() {
+	// map de scroll avec minimum 0 et maximum la taille du body (moins la hauteur de la fenêtre) définie en css
+	let b = map(scroll,0,3000-height,100,255);
+	let w = map(scroll,0,3000-height,0,width);
+  background(100,255,b);
+	fill(0);
+	noStroke();
+	rect(0,height/2,w,10);
+
+}
+
+window.addEventListener("scroll", function (event) {
+    scroll = this.scrollY;
+    console.log(scroll);
+});
